c83cad0df8d3c1eb31d8cecd1746762bb75d3ea6
MashrafiDipto <mashrafidipto36@gmail.com>
2021-12-20 15:20:35 +0600

    lua config

--- lua/dipto/notify.lua
+++ lua/dipto/notify.lua
@@ -1,14 +1,12 @@
-vim.notify = require("notify")
-
 require("notify").setup({
   -- Animation style (see below for details)
   stages = "fade_in_slide_out",
 
   -- Function called when a new window is opened, use for changing win settings/config
-  on_open = nil,
+  on_open = true,
 
   -- Function called when a window is closed
-  on_close = nil,
+  on_close = true,
 
   -- Render function for notifications. See notify-render()
   render = "default",
@@ -32,3 +30,4 @@ require("notify").setup({
     TRACE = "✎",
   },
 })
+
